83dde71a426d6d6b9de0e9bff24acb885d823344
rmitnl <rich.martin@itential.com>
2024-10-28 10:10:38 -0400

    test

--- get_int_counters/get_int_counters.py
+++ get_int_counters/get_int_counters.py
@@ -11,8 +11,8 @@ print(sys.argv[1:])
 device_ip=sys.argv[1]
 inter_name=sys.argv[2]
 
-device = ConnectHandler(device_type='brocade_fastiron', ip=device_ip, username=var_u, password=var_p,secret=var_s)
-output = device.send_command("show statistics "+inter_name)
-print (output)
+#device = ConnectHandler(device_type='brocade_fastiron', ip=device_ip, username=var_u, password=var_p,secret=var_s)
+#output = device.send_command("show statistics "+inter_name)
+#print (output)
 
 device.disconnect()
