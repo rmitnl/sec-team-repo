c5b521d455b61ad17433ad68c48bb5d1f20a9ab5
rmitnl <rich.martin@itential.com>
2024-10-28 10:32:07 -0400

    test

--- get_int_counters/get_int_counters.py
+++ get_int_counters/get_int_counters.py
@@ -2,35 +2,31 @@ import sys
 from vars import *
 from netmiko import ConnectHandler
 
-print(sys.argv[1:])
+#print(sys.argv[1:])
 # check for 2 args
 if len(sys.argv) != 3:
     print('2 args required')
     sys.exit(1)
 
-if sys.argv[1][0:7] != "--device":
+device = sys.argv[1]
+interface = sys.argv[2]
+
+#check for correct args
+if device[0:8] != "--device":
     print('No device ip')
     sys.exit(1)
 
-if sys.argv[2][0:11] != "--interface":
+if interface[0:11] != "--interface":
     print('No interface name')
     sys.exit(1)
 
-device_ip=sys.argv[1]
-inter_name=sys.argv[2]
+# extract variables
+device = device[9:]
+interface = interface[12:]
 
-#device = ConnectHandler(device_type='brocade_fastiron', ip=device_ip, username=var_u, password=var_p,secret=var_s)
-#output = device.send_command("show statistics "+inter_name)
-#print (output)
+print (device,interface)
+network = ConnectHandler(device_type='brocade_fastiron', ip=device, username=var_u, password=var_p,secret=var_s)
+output = network.send_command("show statistics "+interface)
+print (output)
 
-#device.disconnect()
-
-
-args = []
-out='{"args":['
-for i in sys.argv[1:]:
-    if i[0:5] != "--arg":
-        out += '"' + i + "\","
-        args.append(i)
-out = out[:-1] + "]}"
-print(out)
+network.disconnect()
