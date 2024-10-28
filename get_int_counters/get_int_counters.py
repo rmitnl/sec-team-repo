d9f5031c8412c5ac4627925ac140df1dbc9cf149
rmitnl <rich.martin@itential.com>
2024-10-28 10:16:12 -0400

    test

--- get_int_counters/get_int_counters.py
+++ get_int_counters/get_int_counters.py
@@ -7,6 +7,14 @@ if len(sys.argv) != 3:
     print('2 args required')
     sys.exit(1)
 
+if argv[1][0:7] != "--device"
+    print('No device ip')
+    sys.exit(1)
+
+if argv[2][0:11] != "--interface"
+    print('No interface name')
+    sys.exit(1)
+
 print(sys.argv[1:])
 device_ip=sys.argv[1]
 inter_name=sys.argv[2]
@@ -15,4 +23,14 @@ inter_name=sys.argv[2]
 #output = device.send_command("show statistics "+inter_name)
 #print (output)
 
-device.disconnect()
+#device.disconnect()
+
+
+args = []
+out='{"args":['
+for i in sys.argv[1:]:
+    if i[0:5] != "--arg":
+        out += '"' + i + "\","
+        args.append(i)
+out = out[:-1] + "]}"
+print(out)
